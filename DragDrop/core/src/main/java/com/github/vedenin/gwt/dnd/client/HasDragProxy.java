56bae66d35a472fc3b6068c8dc92479914b8db47
slava <vedenin1980@gmail.com>
2018-02-22 00:44:47 +0100

    Fix bug, update structure, change build system, use gwt 2.8.2

---
+++ DragDrop/core/src/main/java/com/github/vedenin/gwt/dnd/client/HasDragProxy.java
@@ -0,0 +1,19 @@
+package com.github.vedenin.gwt.dnd.client;
+
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Convenience interface for creating drag proxies from widgets.
+ * 
+ * @see PickupDragController#setBehaviorDragProxy(boolean)
+ */
+public interface HasDragProxy {
+
+	/**
+	 * Method to return drag proxy widget.
+	 * 
+	 * @return the drag proxy widget
+	 */
+	Widget getDragProxy();
+
+}
